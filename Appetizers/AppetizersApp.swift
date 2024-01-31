678940a3062a02fbed646302f94122042076d49c
Almayo Ibrahim <almayo.online@gmail.com>
2024-01-31 11:20:08 +0300

    Initial Commit

---
+++ Appetizers/AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppetizersApp.swift
+//  Appetizers
+//
+//  Created by almayo ibrahim  on 31/01/2024.
+//
+
+import SwiftUI
+
+@main
+struct AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
